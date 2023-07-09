9d44cb51fe32e71c9989b1b5488c983a83df4837
Ahmed Elsawalhy <mail@yagasoft.com>
2023-07-09 05:16:37 +0300

    Fixed: tool version update.

--- TemplateCodeGeneratorPlugin/Properties/AssemblyInfo.cs
+++ TemplateCodeGeneratorPlugin/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ahmed Elsawalhy (Yagasoft.com)")]
 [assembly: AssemblyProduct("Template-based Code Generator Plugin")]
-[assembly: AssemblyCopyright("Copyright © Ahmed Elsawalhy 2021")]
+[assembly: AssemblyCopyright("Copyright © Ahmed Elsawalhy 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.1.2")]
-[assembly: AssemblyFileVersion("2.4.1.2")]
+[assembly: AssemblyVersion("2.5.1.2")]
+[assembly: AssemblyFileVersion("2.5.1.2")]
